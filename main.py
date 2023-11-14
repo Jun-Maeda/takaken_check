406b54e510bfb41e2d7dfebc14ef7f46f84923ea
maedajun <jun.mermaid.voice@gmail.com>
2023-11-14 20:04:08 +0900

    first commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     # 環境変数の取り込み
     access_token = os.environ['ACCESS_TOKEN']
-
+    # access_token = "test"
     data = check()
     if data != False:
         send_line(access_token, data)
